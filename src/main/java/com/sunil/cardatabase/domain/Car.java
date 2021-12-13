24427d5154bc97c8d41c9da8fef36d3df466a3f9
unknown <kulyanasun@gmail.com>
2021-12-13 02:26:38 +0100

    first commit

--- src/main/java/com/sunil/cardatabase/domain/Car.java
+++ src/main/java/com/sunil/cardatabase/domain/Car.java
@@ -1,9 +1,12 @@
 package com.sunil.cardatabase.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Car {
@@ -14,6 +17,22 @@ public class Car {
 	
 	private String brand, model, color, registerNumber;
 	private int year, price;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "owner")
+	private Owner owner;
+	
+	//Getter and setter
+	
+	public Owner getOwner() {
+	return owner;
+	}
+	
+	public void setOwner(Owner owner) {
+	this.owner = owner;
+	}
+	
 	
 	public Car() {
 		
