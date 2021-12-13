935fcee525d97f4cbf11aa9f17731c253c5781e0
unknown <kulyanasun@gmail.com>
2021-12-13 23:16:19 +0100

    Maria DB Connections

--- src/main/java/com/sunil/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/sunil/cardatabase/CardatabaseApplication.java
@@ -14,37 +14,30 @@ import com.sunil.cardatabase.repository.OwnerRepository;
 @SpringBootApplication
 public class CardatabaseApplication {
 
-	
-	@Autowired	
+	@Autowired
 	private CarRepository repository;
-	
-	@Autowired	
+
+	@Autowired
 	private OwnerRepository orepository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 	}
-	
-	@Bean
-	CommandLineRunner runner(){
-	return args -> {
-		
-		Owner owner1 = new Owner("John" , "Johnson");
-		Owner owner2 = new Owner("Mary" , "Robinson");
-		orepository.save(owner1);
-		orepository.save(owner2);
-		
-		
-	// Save demo data to database
-	repository.save(new Car("Ford", "Mustang", "Red",
-	"ADF-1121", 2017, 59000,owner1));
-	repository.save(new Car("Nissan", "Leaf", "White",
-	"SSJ-3002", 2014, 29000,owner2));
-	repository.save(new Car("Toyota", "Prius", "Silver",
-	"KKO-0212", 2018, 39000,owner1));
-	};
-}
-	
-}
- 
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+
+			Owner owner1 = new Owner("John", "Johnson");
+			Owner owner2 = new Owner("Mary", "Robinson");
+			orepository.save(owner1);
+			orepository.save(owner2);
+
+			// Save demo data to database
+			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
+			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
+			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner1));
+		};
+	}
+
+}
