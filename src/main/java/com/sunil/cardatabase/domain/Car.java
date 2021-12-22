b35cedce05372dd7281b78fbc0a9088e6b21838f
unknown <kulyanasun@gmail.com>
2021-12-23 00:56:31 +0100

    Test Cases Backend

--- src/main/java/com/sunil/cardatabase/domain/Car.java
+++ src/main/java/com/sunil/cardatabase/domain/Car.java
@@ -38,6 +38,16 @@ public class Car {
 		
 	}
 	
+	public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
+		this.registerNumber = registerNumber;
+		this.year = year;
+		this.price = price;
+	}
+	
 	public Car(String brand, String model, String color, 
 			String registerNumber, int year, int price, Owner owner) {
 		super();
@@ -96,6 +106,14 @@ public class Car {
 
 	public void setPrice(int price) {
 		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 	
 	
